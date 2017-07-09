5a8e13d039a113ecd189b9eac16930e2d112765e
DaviRP <davipegoraro@MacBook-Air-de-Davi.local>
2017-07-09 18:45:24 -0300

    delete function
    delete from de redis data base

--- redusers/app.js
+++ redusers/app.js
@@ -53,3 +53,37 @@ app.post("/user/search", function(req,res, next){
     }
   });
 });
+//Adicionar Usuario
+app.get("/user/add" , function(req, res, next){
+  res.render("adduser");
+
+});
+//requisicao de post
+app.post("/user/add" , function(req, res, next){
+  let id=req.body.id;
+  let first_name=req.body.first_name;
+  let last_name=req.body.last_name;
+  let email=req.body.email;
+  let phone=req.body.phone;
+
+  client.HMSET(id,[
+    "first_name", first_name,
+    "last_name", last_name,
+    "email", email,
+    "phone", phone 
+    ], function(err,reply){
+      if(err){
+        console.log(err);
+      }
+      console.log(reply);
+      res.redirect("/");
+    
+  });
+});
+
+//deletar Usuario
+
+app.delete("/user/delete/:id",function(req,res,next){
+  client.del(req.params.id);
+  res.redirect("/");
+});
